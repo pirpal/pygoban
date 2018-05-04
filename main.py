38ec7118de69cddd77335fe12b9e166a3a6b3fbf
pirpal <ppaillere@gmail.com>
2018-05-04 19:34:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# This is PyGo v0.3
+
+# Go interface for :
+# - free play (empty board)
+# - play against computer (gnugo)
+# - analysis (graphical tools)
+# - .sgf file reader
+# - library of tsumegos and joseki problems
+# - interface for brave Python IA developers
+
+# 2018 - peyop78@gmail.com
+# =============================================================================
+
+import tkinter as tk
+
+
+# =============================================================================
+
+class Stone:
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.belongs_to_group = False
+
+    def __repr__(self):
+        """ for console debug """
+        return self.color[0]
+
+
+# =============================================================================
+
+class StoneGroup:
+
+    def __init__(self, stones):
+        self.stones = stones
+
+
+# =============================================================================
+
+class GoApp(tk.Tk):
+
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.parent = parent
+        self.resizable(width=False, height=False)
+        self.geometry("800x580+100+100")
+        self.title("PyGo")
+        self.goban = [[0 for i in range(19)] for i in range(19)]
+        self.offset = 40
+        self.ssize = 20
+        self.game_turn = 0
+        self.colors = ["white", "black"]
+        self.initWidgets()
+        self.drawGoban()
+        self.drawPoints()
+        self.displayCoordinates()
+        self.can.bind("<Button-1>", self.mouseClick)
+        self.can.bind("<Motion>", self.mouseMove)
+        self.can.bind("<Button-2>", self.displayStoneData)
+
+    def initWidgets(self):
+        self.can = tk.Canvas(self, width=480, height=480, bg="#dcb35c")
+        self.can.grid(row=0, column=0)
+
+        self.goban_coords = tk.StringVar()
+        self.screens_frame = tk.Frame(self, bg="blue")
+        self.screens_frame.grid(row=1, column=0)
+        self.coords_screen = tk.Label(
+            self.screens_frame,
+            justify=tk.LEFT,
+            textvariable=self.goban_coords,
+            width=8,  # characters
+            height=3,  # lines
+            bg="#272822",
+            fg="white"
+        )
+        self.coords_screen.grid(row=0, column=0)
+
+    # DISPLAY -----------------------------------------------------------------
+
+    def drawGoban(self):
+        for x in range(18):
+            for y in range(18):
+                self.can.create_rectangle(
+                    self.offset + x * self.ssize,
+                    self.offset + y * self.ssize,
+                    self.offset + x * self.ssize + self.ssize,
+                    self.offset + y * self.ssize + self.ssize
+                )
+
+    def drawStone(self, stone):
+        self.can.create_oval(
+            self.offset + stone.x * self.ssize - self.ssize / 2,
+            self.offset + stone.y * self.ssize - self.ssize / 2,
+            self.offset + stone.x * self.ssize - self.ssize / 2 + self.ssize,
+            self.offset + stone.y * self.ssize - self.ssize / 2 + self.ssize,
+            fill=stone.color
+        )
+
+    def drawStones(self):
+        for x in range(19):
+            for y in range(19):
+                if self.goban[x][y] != 0:
+                    self.drawStone(self.goban[x][y])
+
+    def displayCoordinates(self):
+        numbers = list(range(19, 0, -1))
+        letters = list("ABCDEFGHJKLMNOPQRST")
+        for i in range(1, 20):
+            self.can.create_text(
+              self.offset/2,
+              self.offset/2 + i * self.ssize,
+              text=numbers[i-1]
+            )
+            self.can.create_text(
+              self.offset + self.ssize * 19,
+              self.offset/2 + i * self.ssize,
+              text=numbers[i-1]
+            )
+            self.can.create_text(
+              self.offset/2 + i * self.ssize,
+              self.offset/2,
+              text=letters[i-1]
+            )
+            self.can.create_text(
+                self.offset/2 + i * self.ssize,
+                self.offset + self.ssize * 19,
+                text=letters[i-1]
+            )
+
+    def drawPoint(self, x, y):
+        self.can.create_oval(
+            self.offset + x * self.ssize - 2,
+            self.offset + y * self.ssize - 2,
+            self.offset + x * self.ssize + 2,
+            self.offset + y * self.ssize + 2,
+            fill="black"
+        )
+
+    def drawPoints(self):
+        """ display tengen and hoshi points on the goban """
+        self.drawPoint(3, 3)
+        self.drawPoint(9, 3)
+        self.drawPoint(15, 3)
+        self.drawPoint(3, 9)
+        self.drawPoint(9, 9)
+        self.drawPoint(15, 9)
+        self.drawPoint(3, 15)
+        self.drawPoint(9, 15)
+        self.drawPoint(15, 15)
+
+    def drawLastMove(self, x, y):
+        """ red square on last played stone """
+        self.can.delete("last")
+        self.can.create_rectangle(
+            self.offset + x * self.ssize - 4,
+            self.offset + y * self.ssize - 4,
+            self.offset + x * self.ssize + 4,
+            self.offset + y * self.ssize + 4,
+            outline="red", fill="red", tag="last"
+        )
+
+    # DATA AND DEBUG ----------------------------------------------------------
+
+    def consoleDisplay(self):
+        print("move {}\n".format(self.game_turn + 1))
+        for x in self.goban:
+            print(*x, sep=" ")
+
+    def displayStoneData(self, evt):
+        pass
+
+    def humanCoords(self, x, y):
+        """ goban[x][y] -> 'q13' """
+        return "abcdefghjklmnopqrst"[x] + str(y + 1)
+
+    # GAME LOGIC --------------------------------------------------------------
+
+    def countLiberties(self, stone):
+        liberties = 0
+        neighbours = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+        for n in neighbours:
+            try:
+                if self.goban[stone.x + n[0]][stone.y + n[1]] == 0:
+                    liberties += 1
+                # check if the stone is connected in a group :
+                elif self.goban[stone.x + n[0]][stone.y + n[1]].color == stone.color:
+                    stone.belongs_to_group = True
+            except IndexError:
+                pass
+        return liberties
+
+    def countGroupLiberties(self, stone_group):
+        liberties = 0
+        for stone in stone_group:
+            liberties += self.countLiberties(stone)
+        return liberties
+
+    # EVENTS ------------------------------------------------------------------
+
+    def mouseMove(self, evt):
+        """ display preview stone above the goban """
+        self.can.delete("preview")
+        goban_x = (evt.x - self.offset) // self.ssize
+        goban_y = (evt.y - self.offset) // self.ssize
+        color = self.colors[self.game_turn % 2]
+        if goban_x in range(19) and goban_y in range(19):
+            # update coords strvar
+            self.goban_coords.set(self.humanCoords(goban_x, goban_y))
+            if self.goban[goban_x][goban_y] == 0:
+                self.can.create_rectangle(
+                    self.offset + goban_x * self.ssize - 5,
+                    self.offset + goban_y * self.ssize - 5,
+                    self.offset + goban_x * self.ssize + 5,
+                    self.offset + goban_y * self.ssize + 5,
+                    fill=color, tag="preview"
+                )
+
+    def mouseClick(self, evt):
+        goban_x = (evt.x - self.offset) // self.ssize
+        goban_y = (evt.y - self.offset) // self.ssize
+        self.can.update()
+        color = self.colors[self.game_turn % 2]
+        if goban_x in range(19) and goban_y in range(19):
+            if self.goban[goban_x][goban_y] == 0:
+                self.goban[goban_x][goban_y] = Stone(goban_x, goban_y, color)
+                self.game_turn += 1
+                self.drawStones()
+                self.drawLastMove(goban_x, goban_y)
+        # self.consoleDisplay()
+
+
+# =============================================================================
+if __name__ == '__main__':
+    app = GoApp(None)
+    app.mainloop()
---
+++ tests.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+for i in range(19):
+    print("{} - {} - {}".format(i, 19 - i, i - 19))
